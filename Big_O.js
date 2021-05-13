32c964d0852e1a7cf6acb328f2f039eb2c6d1519
rockshellS <spicer.rachelle@gmail.com>
2021-05-13 15:32:11 -0500

    big O

---
+++ Big_O.js
@@ -0,0 +1,25 @@
+//Big O Definition
+
+
+// we say that an algorithm is O(f(n)) if the number of simple operations the computer
+// has to do is eventually less than a constant times f(n), as n increases
+
+
+                        // this notation below just means a function with an input of n (f(n) and then the output =n) 
+                        //so we are discribing the relationship between the input and the run time
+
+// * f(n) could be linear (f(n)) = n)
+// linear meaning as n scales the input the run time scales as well
+
+                                    // this is n squared
+// * f(n) could be quadratic (f(n) = n2)
+// where n grows the run time squares related to the square event
+
+
+
+
+// * f(n) could be constant )f(n) =1)
+// as n grows it doesn't really have an impact because runtime is always constant which we simplify down to one 
+
+
+// * f(n) could be something entirely different!
